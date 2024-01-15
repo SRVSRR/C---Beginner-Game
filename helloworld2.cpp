92974f965d8f5f59bb510a58488676bc57e0226b
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-15 22:32:49 +1200

    class 2

---
+++ helloworld2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    int age = 21;
+    int year = 2023;
+    double days = 7.5;
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    bool student = true;
+    bool power = true;
+    bool forSale = false;
+
+    std::string name = "Rohan";
+
+
+
+    std::cout << days << std::endl;
+    std::cout << currency << std::endl;
+    std::cout << "Hello, bro im " <<  age ;
+
+    std::cout << "Hello " <<  name;
+
+
+
+
+
+    return 0;
+}
