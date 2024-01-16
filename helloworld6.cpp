229357ffa42ea5837293ab55a84ea0ef923bbed6
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-16 21:08:56 +1200

    class 6

---
+++ helloworld6.cpp
@@ -0,0 +1,28 @@
+//arithmetic operators
+
+#include <iostream>
+
+int main() {
+
+    int students = 6 - 5 + 4 * 3 / 2;
+
+    //students++;
+    //students = students + 1;
+    //students+=1;
+
+    //students--;
+    //students = students - 1;
+    //students-=1;
+
+    //students = students * 2;
+    //students*=2;
+
+    //students = students / 2;
+    //students/=3;
+
+    //int remainder = students % 4;
+
+    std::cout << students;
+
+    return 0;
+}
