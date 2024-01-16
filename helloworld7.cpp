a223573395ac22ef60202cc4bb42776619c375c3
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-16 21:21:03 +1200

    class 7

---
+++ helloworld7.cpp
@@ -0,0 +1,20 @@
+//Type conversion
+
+#include <iostream>
+
+int main(){
+
+    //double x = (int)3.14;
+
+    //char x = 100;
+
+    int correct = 8;
+
+    int questions = 10;
+
+    double score = correct/(double)questions * 100;
+
+    std::cout << score;
+
+    return 0;
+}
