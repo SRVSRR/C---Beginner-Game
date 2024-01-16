ced64460614e97488a216b4b7dd8fbc2bec12912
Rohan Nandan <srvsrr@Rohans-MacBook-Air.local>
2024-01-16 20:22:45 +1200

    class 3

---
+++ helloworld3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    const int LIGHT_SPEED = 299792458;
+
+    std::cout << LIGHT_SPEED << "Light Speed";
+
+    std::cout << circumference << "cm^2";
+
+    return 0;
+}
