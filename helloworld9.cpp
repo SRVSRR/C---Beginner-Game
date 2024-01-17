c4bba93b918353c4c4b3a643cd2e12b09c2ecab5
srvsrr <rohannandan2004@gmail.com>
2024-01-17 21:38:08 +1200

    'class 9'

---
+++ helloworld9.cpp
@@ -0,0 +1,22 @@
+// math related stuff
+
+#include <iostream> 
+#include <cmath>
+
+int main() {
+
+    double x = 3.94;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(5, 3);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = floor(x);
+
+    std::cout << z;
+
+    return 0;
+}
