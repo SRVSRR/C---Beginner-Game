1f2545be8ff18da2bed1e5f395adf3f2e2947930
srvsrr <rohannandan2004@gmail.com>
2024-01-20 00:24:36 +1200

    'class 14'

---
+++ helloworld14.cpp
@@ -0,0 +1,20 @@
+//Ternary operator
+
+#include <iostream>
+
+int main() {
+
+    //int grade = 75;
+    //(grade >= 60) ? std::cout << "You passed" : std::cout << "You failed";
+
+    //int number = 10;
+    //number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
+
+    bool hungry = true;
+
+    //hungry ? std::cout << "Get Food" : std::cout << "NVM";
+
+    std::cout << (hungry ? "Get Food" : "NVM");
+    
+    return 0;
+}
