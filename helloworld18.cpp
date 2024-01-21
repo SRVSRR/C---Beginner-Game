b53ee447403e277be2c5c65a8766955cfe9a8589
srvsrr <rohannandan2004@gmail.com>
2024-01-22 00:05:10 +1200

    'class 18'

---
+++ helloworld18.cpp
@@ -0,0 +1,22 @@
+//while loops
+
+#include <iostream>
+
+int main( ) {
+
+    std::string name;
+
+    /*while(name.empty() ){
+        std::cout << "Enter name: ";
+        std::getline(std::cin, name);
+    }*/
+
+    while(1 == 1){
+        std::cout << "Loop: ";
+    }
+
+    std::cout << "Hello " << name;
+
+
+    return 0;
+}
