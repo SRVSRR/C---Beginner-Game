ebf3fb23678d32e579616be6af3c5e3c8b3e420c
srvsrr <rohannandan2004@gmail.com>
2024-02-27 15:01:29 +1200

    class 25

---
+++ helloworld25.cpp
@@ -0,0 +1,36 @@
+// random number guessor
+
+#include <iostream>
+#include <ctime>
+
+int main() {
+
+    int num;
+    int guess;
+    int tries;
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+    std::cout << "*****Number Guessor*****\n";
+    
+    do{
+        std::cout << "Enter a guess between 1 - 100: "; 
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num){
+            std::cout << "Too High\n";
+        }
+        else if(guess < num){
+        std::cout << "Too Low\n";
+        }
+        else {
+            std::cout << "You are correct\n";
+        }
+
+    } while(guess !=num);
+
+
+    return 0;
+}
