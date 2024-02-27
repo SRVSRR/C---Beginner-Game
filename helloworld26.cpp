ac01568585609db28025ed0e38d03e39e71511a6
srvsrr <rohannandan2004@gmail.com>
2024-02-27 15:30:20 +1200

    class 26

--- helloworld26.cpp
+++ helloworld26.cpp
@@ -1,11 +1,27 @@
-//
+//Usere defined functions
 
 #include <iostream>
-#include <ctime>
+
+void happybirthday(std::string name, int age);
+
 
 int main() {
 
+    std::string name = "Bro";
+    int age = 21;
 
+
+    happybirthday(name, age);
 
     return 0;
 }
+
+void happybirthday(std::string name, int age){
+    std::cout << "Happy birthday to " << name << '\n';
+    std::cout << "Happy birthday to " << name << '\n';
+    std::cout << "Happy birthday to " << name << '\n';
+    std::cout << "You are " << age << "Years old\n";
+}
+
+
+// function = block of reusbale code
