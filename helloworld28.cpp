a408e162562022ce29ecad8968dd148166d62690
srvsrr <rohannandan2004@gmail.com>
2024-04-04 11:41:16 +1200

    'class 28'

--- helloworld28.cpp
+++ helloworld28.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int myNum = 3;
+
+void printNum();
+
 int main() {
+    int myNum = 1;
+
+    printNum();
+    cout << ::myNum << '\n';
 
 
     return 0;
+}
+void printNum(){
+    int myNum = 2;
+
+    cout << ::myNum << '\n';
 }
---
+++ helloworld29.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int masin() {
+
+
+    return 0;
+}
