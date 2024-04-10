68288be0cf9a3d6ec36e18f86b25f9a6e9e864b8
srvsrr <rohannandan2004@gmail.com>
2024-04-11 09:47:32 +1200

    '11/04/24'

--- helloworld29.cpp
+++ helloworld29.cpp
@@ -1,6 +1,7 @@
 //Banking practice program
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,27 +11,43 @@ double withdraw(double balance);
 
 int main() {
 
-    double balance = 0;
+    double balance = 123;
     int choice = 0 ;
+    do{
+        cout << "*****************\n";
+        cout << "Enter your choice\n";
+        cout << "*****************\n";
+        cout << "1 - Show balance\n";
+        cout << "2 - Deposit Money\n";
+        cout << "3 - Withdraw money\n";
+        cin >> choice;
 
-    cout << "Enter your choice\n";
-    cout << "Show balance\n";
-    cout << "Deposit Money\n";
-    cout << "Withdraw money\n";
-    cin >> choice;
+        switch(choice){
+            case 1: showBalance(balance);
+                    break;
+            case 2: balance += deposit();
+                    break;
+            case 3: balance -= withdraw(balance);
+                    break;
+            case 4: cout << "Thanks for visiting\n";
+                    break;
 
-    switch(choice){
-        case 1: showBalance(balance);
-                break;
-        case 2: balance += deposit();
-                break;
-        case 3: balance -= withdraw(balance);
-                break;
-        case 4: cout << "Thanks for visiting\n";
-                break;
-
-        default:cout << "Invalid choice\n";
-    }
+            default:cout << "Invalid choice\n";
+        }
+    }while(choice != 4);
+    
 
     return 0;
+}
+
+void showBalance(double balance){
+    cout << "Your balance is: $" << setprecision(2) << fixed << balance << "\n";
+}
+
+double deposit(){
+    
+    return 0 ;
+}
+double withdraw(double balance){
+    return 0 ;
 }
