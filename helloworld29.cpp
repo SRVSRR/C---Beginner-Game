d06cfb623a8e84dbedb58752254c6e3531739a79
srvsrr <rohannandan2004@gmail.com>
2024-04-21 21:13:11 +1200

    class 29

--- helloworld29.cpp
+++ helloworld29.cpp
@@ -11,7 +11,7 @@ double withdraw(double balance);
 
 int main() {
 
-    double balance = 123;
+    double balance = 0;
     int choice = 0 ;
     do{
         cout << "*****************\n";
@@ -22,17 +22,22 @@ int main() {
         cout << "3 - Withdraw money\n";
         cin >> choice;
 
+        cin.clear();
+        fflush(stdin);
+
         switch(choice){
             case 1: showBalance(balance);
                     break;
             case 2: balance += deposit();
+                    showBalance(balance);
                     break;
             case 3: balance -= withdraw(balance);
+                    showBalance(balance);
                     break;
             case 4: cout << "Thanks for visiting\n";
                     break;
 
-            default:cout << "Invalid choice\n";
+            default: cout << "Invalid choice\n";
         }
     }while(choice != 4);
     
@@ -41,16 +46,37 @@ int main() {
 }
 
 void showBalance(double balance){
-    cout << "Your balance is: $" << setprecision(2) << fixed << balance << "\n";
+    cout << "Your balance is: $" << setprecision(2) << fixed << balance << '\n';
 }
 
 double deposit(){
     double amount = 0;
     cout << "Enter deposit amount: \n";
     cin >> amount;
+
+    if(amount > 0){
+        return amount;
+    } else {
+        cout <<"Thats not a valid amount\n";
+        return 0;
+    }
     
-    return 0 ;
+    
 }
 double withdraw(double balance){
-    return 0 ;
+    double amount = 0;
+    cout << "Enter amount to be withdrawn\n";
+    cin >> amount;
+    if(amount > balance){
+        cout << "Insufficient fund\n";
+        return 0;
+    }
+    else if(amount < 0){
+        cout << "Not valid amount\n";
+        return 0;
+    }
+    else{
+          return amount;
+    }
+  
 }
