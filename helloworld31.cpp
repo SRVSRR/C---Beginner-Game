18dc1dd34610013c8e9dac4b12dbf65d9d05b07e
srvsrr <rohannandan2004@gmail.com>
2024-04-21 22:14:42 +1200

    class 31

---
+++ helloworld31.cpp
@@ -0,0 +1,29 @@
+// Arrays
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*string car[3];
+
+    car[0] = "corvette";
+    car[1] = "mustang";
+    car[2] = "camry";
+
+    cout << car[0] << '\n';
+    cout << car[1] << '\n';
+    cout << car[2] << '\n';
+    */
+
+   double prices[] ={5.00, 7.00, 9.00};
+
+    cout << prices[0] << '\n';
+    cout << prices[1] << '\n';
+    cout << prices[2] << '\n';
+
+
+    return 0;
+}
+
