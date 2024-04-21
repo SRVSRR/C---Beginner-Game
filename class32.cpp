3530a45f5332c614908e671445a46d44db900a76
srvsrr <rohannandan2004@gmail.com>
2024-04-22 08:48:45 +1200

    class 32 and 35

--- class32.cpp
+++ class32.cpp
@@ -1,1 +1,20 @@
 // size of operator
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name = "Mew dhdhih dsihs";
+    double gpa = 2.5;
+    char grade = 'f';
+    bool stud = true;
+    char grades[] = {'A','B','C','D','F'};
+    string studs[] = {"std","dsudh","dshjd","dsbhdb"};
+
+    cout << sizeof(studs)/sizeof(string) << " bytes\n";
+
+
+    return 0;
+}
---
+++ class35.cpp
@@ -0,0 +1,19 @@
+//iterate over an array
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    char grade[] = {'a', 'b', 'c', 'd', 'f'};
+
+    /*string students[] = {"bob", "jib","ken", "rijj"};*/
+
+    for(int i = 0;i < sizeof(grade)/sizeof(char); i++){
+        cout << grade[i] << '\n';
+    }
+
+
+    return 0;
+}
