42a36ea8d7f703a3dde8650196b4fce6db2153ab
srvsrr <rohannandan2004@gmail.com>
2024-04-22 17:32:30 +1200

    37

---
+++ class37.cpp
@@ -0,0 +1,17 @@
+// Pass array to a function
+
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+
+    double prices[]= {34.43,434.43,431.33,43.99};
+
+    double total = getTotal
+
+
+    return 0;
+}
+
+double getTotal
